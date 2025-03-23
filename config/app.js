262387c538ed78d026dbfeb56c4fdac7e4056b27
rajeshsomarajannair@pvh.com <rajeshsomarajannair@pvh.com>
2025-03-23 02:55:59 +0100

    added chit chat feature

--- config/app.js
+++ config/app.js
@@ -27,7 +27,7 @@ module.exports = {
   | to encrypted cookies, sessions and other sensitive data.
   |
   */
-  appKey: Env.getOrFail('APP_KEY'),
+  appKey: Env.get('APP_KEY', 'default-fallback-key'),
 
   http: {
     /*
